fc2646e8930ba21d082a3dd38fc3ba7a3214223e
gold106qq <113621093+gold106qq@users.noreply.github.com>
2022-09-18 14:09:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "吴某某",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oZrGs6Tstfo0hZ_XHP052y0Yf8Sg",
+      id: "oZrGs6XPho2ke-3PCquHorSUWlsQ",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "c_Oq7wuqqUetOBdKxeyFRkOSsvzaaE_TNxSRJgbwhAY",
       // 所在省份
@@ -74,7 +74,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
