4a84eea73be91f42a461ca499b0b1779b8087a22
gold106qq <113621093+gold106qq@users.noreply.github.com>
2022-09-18 10:56:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oZrGs6Tstfo0hZ_XHP052y0Yf8Sg",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "gGJNAtfwnqFBmQEhcsvzXWBO7K0Cdncqy-imZrGeWes",
+      useTemplateId: "dgMKj4jZogW42cLJThaVl0pq8l2erfptpsB-sXacKZg",
       // 所在省份
       province: "贵州",
       // 所在城市
